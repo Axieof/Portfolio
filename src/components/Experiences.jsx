b22640dc5797c260dbd0ed680917f52db63334b6
Axieof <pritheev24@gmail.com>
2023-01-01 20:57:45 +0800

    Modified Experiences padding

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -4,7 +4,8 @@ import ExperiencesItem from "./ExperiencesItems";
 
 function Experiences() {
     return (
-        <div className="flex flex-col md:flex-row items-center justify-center">
+        <div className="flex flex-col md:flex-row 
+        items-center justify-center pb-7">
 
             <div className="grid grid-cols-1 gap-4">
                 {experiences.map(project => (
